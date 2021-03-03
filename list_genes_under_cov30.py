eec833626617e641c263caa26269368fa1d72410
seikom <seiko.makino@nhs.net>
2021-03-03 09:06:31 +0000

    initial commit

---
+++ list_genes_under_cov30.py
@@ -0,0 +1,27 @@
+### script to report a list of genes with less than 30x at coverage
+
+### Date: 2021/03
+### authour: Seiko Makino
+### usage: list_genes_cov30.py <sambamba_output>
+
+import os
+import sys
+import argsparse
+
+### parse a argument
+parser = argparse.ArgumentParser(description='Report a list of genes with less than 100% at coverage 30x')
+parser.add_argument('--file', '-f', help='sambamba output file for coverage analysis')
+args = parser.parse_args()
+
+file=args.file
+
+### read in a sambamba output file
+
+f = open(file,'r')
+
+
+### identify rows with less than 100% at cov30
+
+### create a list of genes
+
+### write a report as a text file
